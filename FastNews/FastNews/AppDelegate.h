af02e09fddf6f2855ffb68bd5f989520bd001bc3
Constantin Zinovyev <constzinovyev@gmail.com>
2016-05-06 12:12:33 +0300

    Initial commit

---
+++ FastNews/FastNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastNews
+//
+//  Created by Сonstantin Zinovyev on 06.05.16.
+//  Copyright © 2016 ConstZin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FastNews/FastNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastNews
+//
+//  Created by Сonstantin Zinovyev on 06.05.16.
+//  Copyright © 2016 ConstZin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
