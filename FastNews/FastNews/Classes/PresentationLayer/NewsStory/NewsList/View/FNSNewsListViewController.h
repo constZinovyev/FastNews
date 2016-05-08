bbde882917f4e0f582d039b162f0eea94fd20f50
Constantin Zinovyev <constzinovyev@gmail.com>
2016-05-08 23:49:07 +0300

    Добавлен news storyboard

--- FastNews/FastNews/Classes/PresentationLayer/NewsStory/NewsList/View/FNSNewsListViewController.h
+++ FastNews/FastNews/Classes/PresentationLayer/NewsStory/NewsList/View/FNSNewsListViewController.h
@@ -1,13 +1,13 @@
 //
-//  FNSNewsDetailViewController.m
+//  FNSNewsListViewController.h
 //  FastNews
 //
 //  Created by Сonstantin Zinovyev on 08.05.16.
 //  Copyright © 2016 ConstZin. All rights reserved.
 //
 
-#import "FNSNewsDetailViewController.h"
+#import <UIKit/UIKit.h>
 
-@implementation FNSNewsDetailViewController
+@interface FNSNewsListViewController : UITableViewController
 
 @end
